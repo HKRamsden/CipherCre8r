85af33418bbcf0a48314b4fc32b0de122755ed68
Hannah Ramsden <RAMSDENH@my.erau.edu>
2024-04-04 19:34:35 -0400

    Initial Welcome Page

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 ## Imports ##
 from tkinter import *
 
+# Saving Color Codes #
+eerieBlack = "#1D1E18"
+feldgrau = "#445745"
+resedaGreen = "#58735B"
+mint = "#6B8F71"
+
+# Creating Root Window #
 root = Tk()
+root.title("CipherCr8r")
+root.geometry('500x500')
+root.configure(bg=eerieBlack)
+
+# Execute Tkinter
+root.mainloop()
