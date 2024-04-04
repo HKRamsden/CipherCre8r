84c74778279d23b9b654f6ff74d43358a684eeb7
Hannah Ramsden <RAMSDENH@my.erau.edu>
2024-04-04 19:55:23 -0400

    AES Beginning Stage

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 ## Imports ##
 from tkinter import *
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+
 
 # Saving Color Codes #
 eerieBlack = "#1D1E18"
@@ -12,6 +16,18 @@ root = Tk()
 root.title("CipherCr8r")
 root.geometry('500x500')
 root.configure(bg=eerieBlack)
+
+
+
+
+
+## AES Encryption ##
+data = b'secret data'
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest(data)
+nonce = cipher.nonce
+print(ciphertext)
 
 # Execute Tkinter
 root.mainloop()
